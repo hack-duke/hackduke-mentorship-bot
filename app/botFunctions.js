c9cbb5675ad0953ba2b989dbd89075be69a3cb11
Andy <ownz.andy@gmail.com>
2016-10-01 05:44:14 -0400

    initial implementation with mentor search, slack group creation + invitation

---
+++ app/botFunctions.js
@@ -0,0 +1,90 @@
+var request_manager = require('./apiRequestManager.js')
+var botkitMongoStorage = require('../config/botkitMongoStorage')({mongoUri: process.env.MONGOURI})
+
+exports.setUpDialog = function(controller) {
+
+  // pulls mentors from registration API and updates them on the database
+  controller.hears('update mentors','direct_message,direct_mention',function(bot,message) {
+    request_manager.getMentors(function(err, body) {
+      for(var i = 0; i < body.length; i++){
+        var mentor = body[i];
+        // arrange data to be placed in Mentor schema
+        data = {
+          first_name: mentor['person']['first_name'],
+          last_name: mentor['person']['last_name'],
+          email: mentor['person']['email'],
+          skills: mentor['role']['skills'],
+          active: false,
+          available: true,
+          slack_id: 'U0Q56HGFR'
+        }
+        botkitMongoStorage.mentors.save(data, function(err, result) {
+          if(err || !result) {
+            bot.reply(message, JSON.stringify(err));
+          } 
+        });
+      }
+      bot.reply(message, 'Mentors updated!')
+    });
+  });
+
+  // according to the skill/topic asked about, find a mentor, create a group, and invite all parties
+  controller.hears('help (.*)', 'direct_message,direct_mention', function(bot,message) {
+    var skills = ['frontend', 'mobile', 'backend', 'web', 'math'];
+    var skill = message.match[1];
+    // skill inputted must match one of the ones listed above, should move to database
+    if(skills.indexOf(skill.trim().toLowerCase()) == -1) {
+      return bot.reply(message, `Please select one of the following skills: ${skills.join(', ')}`);
+    }
+    botkitMongoStorage.mentors.startSession(skill, function(err, result) {
+      if(err || !result) {
+        return bot.reply(message, err);
+      }
+      var mentorSlackId = result['slack_id'];
+      var participantSlackId = message['user'];
+      // group name is currently lowercase mentor id, dash, and lowercase participant id
+      var groupName = mentorSlackId.toLowerCase() + '-' + participantSlackId.toLowerCase();
+      request_manager.createGroup(groupName, function(err, body) {
+        if(err || !result) {
+          return bot.reply(message, JSON.stringify(err));
+        } 
+        // only reply with error if there's an error that's not name_taken
+        // if name_taken, a group between the two users should've started before
+        // and you can simply invite them to the group again if necessary
+        if(!body['ok'] && body['error'] != 'name_taken') {
+          bot.reply(message, body['error']);
+        } else {
+          // uppercase ids must be passed into inviteToGroup
+          request_manager.inviteToGroup(groupName, participantSlackId, mentorSlackId, function(err, result) {
+            if(err || !result) {
+              return bot.reply(message, JSON.stringify(err));
+            }
+            return bot.reply(message, 'Mentor assigned');
+          });
+        } 
+      });
+    });
+  });
+
+  controller.hears('end session','direct_message,direct_mention',function(bot,message) {
+    botkitMongoStorage.mentors.endSession(message['user'], function(err, result) {
+      if(err || !result) {
+        return bot.reply(message, err);
+      } else {
+        return bot.reply(message,'Thanks for ending the session, you\'ve been placed back into the queue.');
+      }
+    });
+  });
+
+  controller.on('direct_message,mention,direct_mention',function(bot,message) {
+    bot.api.reactions.add({
+      timestamp: message.ts,
+      channel: message.channel,
+      name: 'robot_face',
+    },function(err) {
+      if (err) { console.log(err) }
+      bot.reply(message,'I heard you loud and clear boss.');
+    });
+  });
+
+}
---
+++ config/botkitMongoStorage.js
@@ -0,0 +1,89 @@
+var mongoose = require('mongoose');
+
+module.exports = function(config) {
+
+    if (!config || !config.mongoUri)
+        throw new Error('Need to provide mongo address.');
+
+    mongoose.Promise = global.Promise
+    mongoose.connect(config.mongoUri)
+
+    var mentorSchema = new mongoose.Schema({ 
+        first_name: String, 
+        last_name: String,
+        email: String,
+        skills: [String],
+        active: Boolean, // whether the mentor is currently mentoring or not
+        available: Boolean, // whether the mentor is open to judging at the moment
+        slack_id: String
+    });
+
+    var Mentor = mongoose.model('Mentor', mentorSchema);
+
+    var unwrapFromList = function(cb) {
+        return function(err, data) {
+            if (err) return cb(err);
+            cb(null, data);
+        };
+    };
+
+    var storage = {
+        mentors: {
+            get: function(email, cb) {
+                Mentor.findOne({email: email}, function(err, result) {
+                    cb(null, result);
+                });
+            },
+            // saves mentor to database
+            save: function(data, cb) {
+                Mentor.findOneAndUpdate({email: data.email}, data, {upsert:true}, function(err, result) {
+                    cb(null, result);
+                })
+            },
+            all: function(cb) {
+                Mentor.find({}, cb);
+            },
+            // sets availability of mentor
+            setAvailability: function(slackId, availability, cb) {
+                Mentor.findOne({slack_id: slackId}, function(err, result) {
+                    if(!result || err) {
+                        cb('You aren\'t a mentor', null);
+                    } else {
+                        Mentor.update({email: result.email}, {available: setAvailability}, {upsert:true}, function(err, result) {
+                            cb(null, result);
+                        });
+                    }
+                });
+            },
+            // ends mentorship session by setting active to false
+            endSession: function(slackId, cb) {
+                Mentor.findOne({slack_id: slackId, active: true}, function(err, result) {
+                    if(!result || err) {
+                        cb('You don\'t have a mentorship session to end', null);
+                    } else {
+                        Mentor.update({email: result.email}, {active: false}, {upsert:true}, function(err, result) {
+                            cb(null, result);
+                        })
+                    }
+                });
+            },
+            // starts a mentorship session by finding a mentor by matching skills and setting the active to true
+            startSession: function(skill, cb) {
+                // capitalizes the skill because they're store in the database capitalized (Python, Swift, etc.)
+                var capitalized = skill.charAt(0).toUpperCase() + skill.substring(1).toLowerCase();
+                Mentor.findOne({skills: capitalized, available: true, active: false}, function(err, result) {
+                    if(!result || err) {
+                        cb('No mentors available for that skill, please try again later', result);
+                    } else {
+                        var mentor = result;
+                        Mentor.update({email: result.email}, {active: true}, {upsert:true}, function(err, result) {
+                            cb(null, mentor);
+                        })
+                    }
+                });
+            }
+        },
+    };
+
+    return storage;
+};
